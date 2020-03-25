2f30eb5affa5daaede1904cc947cc610bde5980a
japhari mbaru <japharimbaru@gmail.com>
2020-03-25 16:37:14 +0300

    adding dialog flow

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
+from utils import fetch_reply
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Jeph Bot!"
 
 
 @app.route("/sms", methods=['POST'])
@@ -14,10 +16,12 @@ def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
+    phone_no = request.form.get('From')
+    reply = fetch_reply(msg, phone_no)
 
     # Create reply
     resp = MessagingResponse()
-    resp.message("Am Japhari You said: {}".format(msg))
+    resp.message(reply)
 
     return str(resp)
 
